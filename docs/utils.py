fb2e20323de30e28e5b3a8c6e616c798510aecd8
Zhou Liu <zhou@Zhous-MacBook-Pro.local>
2024-08-02 13:13:18 -0400

    change image path

--- docs/utils.py
+++ docs/utils.py
@@ -13,7 +13,7 @@ def extract_images_from_excel(excel_file):
     sheet = wb[SHEET_NAME]
     image_loader = SheetImageLoader(sheet)
 
-    image_dir = 'docs/static/images'
+    image_dir = 'static/images'
     if not os.path.exists(image_dir):
         os.makedirs(image_dir)
 
